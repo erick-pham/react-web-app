f29a8f9dca0d12f84b5570d8328c701dd194c763
Erick.Pham <toan.pham@unicorn-fintech.com>
2019-06-03 17:33:47 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import People from './components/People';
 
 function App() {
+  const people = [{
+    personNo: '1',
+    firstName: 'Bill',
+    lastName: 'Gates'
+  },
+  {
+    personNo: '2',
+    firstName: 'Donald',
+    lastName: 'Trump'
+  },
+  {
+    personNo: '3',
+    firstName: 'Erick',
+    lastName: 'Pham'
+  },
+  {
+    personNo: '3',
+    firstName: 'Erick',
+    lastName: 'Pham'
+  }, {
+    personNo: '3',
+    firstName: 'Erick',
+    lastName: 'Pham'
+  }, {
+    personNo: '3',
+    firstName: 'Erick',
+    lastName: 'Pham'
+  }];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid">
+      <Header />
+
+      <People people={people} />
     </div>
   );
 }
---
+++ src/components/Person/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './style.css';
+export default class Person extends Component {
+  constructor(props) {
+    super(props);
+  }
+  // Use the render function to return JSX component
+  render() {
+    return (
+      <div className='person-info'>
+        <h3>Person {this.props.person.personNo}:</h3>
+        <ul>
+          <li>First Name: {this.props.person.firstName}</li>
+          <li>Last Name: {this.props.person.lastName}</li>
+        </ul>
+      </div>
+    );
+  }
+}
